06ea025cd2eff1bde8aae16d41ec7f5ead8a5f61
MagalhaesDev <mateusmagalhaesemidio@gmail.com>
2022-10-02 10:29:39 -0300

    page product

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,25 @@
+import Image from 'next/image'
 import { useRouter} from 'next/router'
+import { ImageContainer, ProductContainer, ProductDetails } from '../../styles/pages/product'
 
 export default function Product() {
     const { query } = useRouter()
 
     return (
-        <h1>Product: {JSON.stringify(query)}</h1>
+        <ProductContainer>
+            <ImageContainer>
+            </ImageContainer>
+
+            <ProductDetails>
+                <h1>Casmiseta X</h1>
+                <span>R$ 79,90</span>
+
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit, error. Hic accusantium itaque numquam facilis maxime iusto labore maiores praesentium eos? Saepe repellat provident quas. Maiores quod sint quia ea.</p>
+           
+                <button>
+                    Comprar agora
+                </button>
+            </ProductDetails>
+        </ProductContainer>
     )
 }
